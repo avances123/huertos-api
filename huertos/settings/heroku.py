2b91f0da0b3f67a6ae0093974652c091b7162702
Fabio Rueda <avances123@gmail.com>
2015-05-28 17:08:07 +0200

    static sin confianza ninguna

--- huertos/settings/heroku.py
+++ huertos/settings/heroku.py
@@ -14,10 +14,11 @@ ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
 # import os
-# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-# STATIC_ROOT = 'staticfiles'
-# STATIC_URL = '/static/'
+BASE_DIR = os.path.dirname('..')
+#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
