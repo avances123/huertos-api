5942f5892b647f608b7c90aae73fab9e29541bb0
Fabio Rueda <avances123@gmail.com>
2015-05-28 18:42:34 +0200

    travis

--- huertos/settings/ci.py
+++ huertos/settings/ci.py
@@ -2,7 +2,8 @@ from huertos.settings.base import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'huertos',                      
         'HOST': '',
         'PORT': 5432
